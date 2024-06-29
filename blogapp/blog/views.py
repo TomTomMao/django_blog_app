95902ef9a859f3f5e7eccab81eae6a2790147f2b
Wentao Mao <118118504+TomTomMao@users.noreply.github.com>
2024-06-29 15:45:30 +0800

    added index view

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Blog, BlogAuthor
 # Create your views here.
+def index(request):
+    num_blogs = Blog.objects.all().count()
+    num_blogAuthors = BlogAuthor.objects.all().count()
+    print(num_blogs)
+    context = {
+        'num_blogs': num_blogs,
+        'num_blogAuthors': num_blogAuthors
+    }
+    return render(request, 'index.html', context=context)
